7b1d6bb4506c1ea7b5b1ee0daca72c6c60ffb17b
cinxdy <cinxdys2@gmail.com>
2021-01-19 13:50:21 +0000

    Updated perror.c

--- ch1_error.c
+++ ch1_error.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <sys/fcntl.h>
+#include <sys/unistd.h>
 
 //void perror (const char *str);
 
 int main(){
-    if(close(fd)=-1)
+    int fd = open("./hello.txt", O_RDONLY);
+    if(close(fd)==-1)
         perror("close");
     return 0;
 }
